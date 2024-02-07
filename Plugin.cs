b2db35b2aceb47be74ae739a41ff001658ba44e2
AzumattDev <azumattgaming@gmail.com>
2024-02-07 11:02:02 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace NoCreatureDust
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoCreatureDustPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "NoCreatureDust";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = $"{Author}.{ModName}";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource NoCreatureDustLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0,
+        }
+
+        public void Awake()
+        {
+            RemoveAllVFX = config("1 - General", "Remove All Effects", Toggle.Off, "Removes all visual effects from when a creature dies, not just the vanilla dust.");
+            RemoveAllRagdollVFX = config("1 - General", "Remove All Ragdoll Effects", Toggle.Off, "Removes all ragdoll effects from creatures, not just the vanilla dust. Just in case mods add more you want to remove.");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        #region ConfigOptions
+
+        public static ConfigEntry<Toggle> RemoveAllVFX = null!;
+        public static ConfigEntry<Toggle> RemoveAllRagdollVFX = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description)
+        {
+            return config(group, name, value, new ConfigDescription(description));
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order = null!;
+            [UsedImplicitly] public bool? Browsable = null!;
+            [UsedImplicitly] public string? Category = null!;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() => "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
+        }
+
+        #endregion
+    }
+
+
+    [HarmonyPatch(typeof(Character), nameof(Character.OnDeath))]
+    static class CharacterOnDeathPatch
+    {
+        public static void Prefix(Character __instance)
+        {
+            if (__instance.IsPlayer() || __instance.m_deathEffects == null || NoCreatureDustPlugin.RemoveAllVFX.Value == NoCreatureDustPlugin.Toggle.Off)
+            {
+                return;
+            }
+
+            // Remove the vfx from the deathEffects and only the vfx
+            EffectList newEffects = new EffectList();
+            foreach (EffectList.EffectData? effect in __instance.m_deathEffects.m_effectPrefabs)
+            {
+                if (effect.m_prefab.name.Contains("vfx_"))
+                {
+                    continue;
+                }
+
+                newEffects.m_effectPrefabs.AddItem(effect);
+            }
+
+            __instance.m_deathEffects = newEffects;
+        }
+    }
+
+    [HarmonyPatch(typeof(Ragdoll), nameof(Ragdoll.DestroyNow))]
+    static class RagdollDestroyNowPatch
+    {
+        static void Prefix(Ragdoll __instance)
+        {
+            if (__instance.m_removeEffect == null)
+            {
+                return;
+            }
+
+            // Remove the vfx from the deathEffects and only the vfx
+            EffectList newEffects = new EffectList();
+            foreach (EffectList.EffectData? effect in __instance.m_removeEffect.m_effectPrefabs)
+            {
+                if (effect.m_prefab.name.Contains("vfx_corpse"))
+                {
+                    continue;
+                }
+
+                if (effect.m_prefab.name.Contains("vfx_") && NoCreatureDustPlugin.RemoveAllRagdollVFX.Value == NoCreatureDustPlugin.Toggle.On)
+                {
+                    continue;
+                }
+
+                newEffects.m_effectPrefabs.AddItem(effect);
+            }
+
+            __instance.m_removeEffect = newEffects;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,70 @@
+# Description
+
+## NoCreatureDust is a mod for Valheim that aims to enhance the player's experience by providing options to remove the visual effects (VFX) seen when creatures die. Specifically, by default it disables the default "dust" effects, with further options to remove additional ragdoll and death effects that may be added by the game or other mods.
+
+`Client Only Mod`
+
+---
+
+## Features/Config Options
+- **Remove All Effects**: Option to remove all visual effects from when a creature dies, not just the vanilla dust. This feature is intended for players who seek a cleaner visual experience during gameplay.
+  - BONUS Side Effect: This feature when on, technically makes the loot instantly appear on the ground when a creature dies, rather than waiting for the death animation to finish. This can be useful for players who want to quickly loot creatures and move on, or for players who are experiencing lag or performance issues when creatures die.
+- **Remove All Ragdoll Effects**: Provides the ability to remove all ragdoll effects from creatures, offering a way to further reduce visual clutter and potentially improve game performance on lower-end systems.
+
+## Compatibility
+- Designed to be compatible with most other mods. However, mods that significantly alter creature death mechanics may conflict (not tested with many mods). Though should be more compatible than the other alternative instant loot mods.
+
+<details>
+<summary><b>Installation Instructions</b></summary>
+
+***You must have BepInEx installed correctly! I can not stress this enough.***
+
+### Manual Installation
+
+`Note: (Manual installation is likely how you have to do this on a server, make sure BepInEx is installed on the server correctly)`
+
+1. **Download the latest release of BepInEx.**
+2. **Extract the contents of the zip file to your game's root folder.**
+3. **Download the latest release of NoCreatureDust from Thunderstore.io.**
+4. **Extract the contents of the zip file to the `BepInEx/plugins` folder.**
+5. **Launch the game.**
+
+### Installation through r2modman or Thunderstore Mod Manager
+
+1. **Install [r2modman](https://valheim.thunderstore.io/package/ebkr/r2modman/)
+   or [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager).**
+
+   > For r2modman, you can also install it through the Thunderstore site.
+   ![](https://i.imgur.com/s4X4rEs.png "r2modman Download")
+
+   > For Thunderstore Mod Manager, you can also install it through the Overwolf app store
+   ![](https://i.imgur.com/HQLZFp4.png "Thunderstore Mod Manager Download")
+2. **Open the Mod Manager and search for "NoCreatureDust" under the Online
+   tab. `Note: You can also search for "Azumatt" to find all my mods.`**
+
+   `The image below shows VikingShip as an example, but it was easier to reuse the image.`
+
+   ![](https://i.imgur.com/5CR5XKu.png)
+
+3. **Click the Download button to install the mod.**
+4. **Launch the game.**
+
+</details>
+
+<br>
+<br>
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/qhr2dWNEYq)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
